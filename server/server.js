ccaed1544a5842e89f17925aa89aac4979d76b84
newprice247 <newprice247@gmail.com>
2023-11-27 10:58:19 -0500

    m

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
@@ -9,12 +11,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.use(routes);
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+
+app.use("/", routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
